d11fa3ce2e32633e26bd451b2a3e87012fdeec7f
parmidayousefi <yousefiparmida84@gmail.com>
2025-06-11 16:00:10 +0330

    Create Task.java

---
+++ app/src/androidTest/java/com/example/project1/Task.java
@@ -0,0 +1,11 @@
+package com.example.project1;
+
+public class Task {
+    public String title;
+    public boolean isDone;
+
+    public Task(String title, boolean isDone) {
+        this.title = title;
+        this.isDone = isDone;
+    }
+}
