71259f62cd86f74320f92ad37530c3d8d876178c
parmidayousefi <yousefiparmida84@gmail.com>
2025-06-12 11:47:24 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,6 @@
-dependencies {
-    classpath("com.google.gms:google-services:4.3.15") // آخرین نسخه
-}
-
-
 plugins {
-    alias(libs.plugins.android.application)
-id("com.google.gms.google-services")
-
+    id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -19,7 +13,6 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -32,24 +25,30 @@ android {
             )
         }
     }
+
+    // این خیلی مهمه برای جاوا:
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
 dependencies {
-implementation("com.google.firebase:firebase-auth:22.3.0")
-implementation("com.google.android.gms:play-services-auth:21.0.0")
+    // Firebase Auth
+    implementation("com.google.firebase:firebase-auth:22.3.0")
 
+    // Google Sign-In
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
 
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    // Java-based dependencies (برای پروژه جاوا)
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
-apply(plugin = "com.google.gms.google-services")
+
+
 
