a199742afaee8de3ade31e09ace2707332d1cad6
parmidayousefi <yousefiparmida84@gmail.com>
2025-06-12 11:52:56 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,53 +1,19 @@
-plugins {
-    id("com.android.application")
-    id("com.google.gms.google-services")
-}
-
-android {
-    namespace = "com.example.project1"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.example.project1"
-        minSdk = 27
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-
-    // این خیلی مهمه برای جاوا:
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+    plugins {
+        id("com.google.gms.google-services") version "4.3.15"
     }
 }
 
-dependencies {
-    // Firebase Auth
-    implementation("com.google.firebase:firebase-auth:22.3.0")
-
-    // Google Sign-In
-    implementation("com.google.android.gms:play-services-auth:21.0.0")
-
-    // Java-based dependencies (برای پروژه جاوا)
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 
