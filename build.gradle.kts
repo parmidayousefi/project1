f7394636207f248004acc47169b09f2f772a3343
parmidayousefi <yousefiparmida84@gmail.com>
2025-06-12 13:01:43 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
+// Top-level build file
+
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.3.15")
+    }
 }
