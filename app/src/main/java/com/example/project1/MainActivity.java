51f730257c0e270ffa2f5e0dfe1805417746b09c
parmidayousefi <yousefiparmida84@gmail.com>
2025-06-12 13:07:02 +0330

    Update MainActivity.java

--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.project1;
 
-import android.os.Bundle;
-import android.view.MenuItem;
-import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
-    public class MainActivity extends AppCompatActivity {
 
-    private static final int RC_SIGN_IN = 9001;
+    private static final int RC_SIGN_IN = 1001;
     private GoogleSignInClient mGoogleSignInClient;
     private FirebaseAuth mAuth;
 
@@ -21,14 +32,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Configure Google Sign-In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id)) // اینو از google-services.json بگیر
+                .requestIdToken(getString(R.string.default_web_client_id)) // defined in strings.xml
                 .requestEmail()
                 .build();
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        mAuth = FirebaseAuth.getInstance();
 
+        // Set up sign-in button
         SignInButton signInButton = findViewById(R.id.sign_in_button);
         signInButton.setOnClickListener(view -> signIn());
     }
@@ -39,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == RC_SIGN_IN) {
@@ -48,7 +63,8 @@ public class MainActivity extends AppCompatActivity {
                 GoogleSignInAccount account = task.getResult(ApiException.class);
                 firebaseAuthWithGoogle(account.getIdToken());
             } catch (ApiException e) {
-                Toast.makeText(this, "Sign in failed: " + e.getStatusCode(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Google sign-in failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.e("MainActivity", "Google sign-in failed", e);
             }
         }
     }
@@ -59,61 +75,12 @@ public class MainActivity extends AppCompatActivity {
                 .addOnCompleteListener(this, task -> {
                     if (task.isSuccessful()) {
                         FirebaseUser user = mAuth.getCurrentUser();
-                        Toast.makeText(this, "Welcome " + user.getDisplayName(), Toast.LENGTH_SHORT).show();
-                        // برو به صفحه اصلی یا کاری انجام بده
+                        Toast.makeText(MainActivity.this, "Welcome, " + user.getDisplayName(), Toast.LENGTH_SHORT).show();
+                        // TODO: انتقال به صفحه اصلی یا Fragment اصلی پروژه
                     } else {
-                        Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
                     }
                 });
     }
 }
 
-
-    BottomNavigationView bottomNavigationView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, new ToDoFragment()).commit();
-
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment selected = null;
-              bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        Fragment selected = null;
-
-        int id = item.getItemId();
-
-        if (id == R.id.nav_todo) {
-            selected = new ToDoFragment();
-        } else if (id == R.id.nav_upload) {
-            selected = new UploadFragment();
-        } else if (id == R.id.nav_profile) {
-            selected = new ProfileFragment();
-        }
-
-        if (selected != null) {
-            getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, selected)
-                .commit();
-            return true;
-        }
-
-        return false;
-    }
-});
-
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.fragment_container, selected).commit();
-                return true;
-            }
-        });
-    }
-}
